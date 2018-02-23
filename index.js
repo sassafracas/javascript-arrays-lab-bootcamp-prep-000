7833920e7433249a3513cc13be2fafba5e267d33
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-23 18:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendKitten(name){
   return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
 }
